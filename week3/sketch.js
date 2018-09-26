32addb3dce08bb37dcefbe10936aab05e4c07b47
Chuyi Zhang <cz1302@nyu.edu>
2018-09-26 11:46:41 -0400

    week3
    A translation/re-creation experiment.

---
+++ week3/sketch.js
@@ -0,0 +1,139 @@
+const apiKey = "AIzaSyCGjsbbjtxUAMcgyehRvWZ7ndCj_1tVqw0";
+const poem =
+  "Do not go gentle into that good night, *\
+Old age should burn and rave at close of day;  *\
+Rage, rage against the dying of the light. * \
+Though wise men at their end know dark is right,  *\
+Because their words had forked no lightning they  *\
+Do not go gentle into that good night. * \
+Good men, the last wave by, crying how bright  *\
+Their frail deeds might have danced in a green bay,  *\
+Rage, rage against the dying of the light. *\
+Wild men who caught and sang the sun in flight, *\
+And learn, too late, they grieved it on its way, *\
+Do not go gentle into that good night. *\
+Grave men, near death, who see with blinding sight *\
+Blind eyes could blaze like meteors and be gay, *\
+Rage, rage against the dying of the light. *\
+And you, my father, there on the sad height,*\
+Curse, bless, me now with your fierce tears, I pray. *\
+Do not go gentle into that good night. *\
+Rage, rage against the dying of the light.";
+
+let arr = [];
+let lines = [];
+let button;
+let content;
+let container;
+let languages = [];
+let lans = [];
+let colors = [];
+let tranlans = [];
+
+function setup() {
+  noCanvas();
+  tranlans.push("en");
+  colors = ["#ffcdcd", "#23a393", "#f1c550", "#ea4c4c", "#774898"];
+  languages = [
+    "arabic",
+    "bulgarian",
+    "chinese",
+    "czech",
+    "danish",
+    "dutch",
+    "english",
+    "french",
+    "german",
+    "italian",
+    "japanese",
+    "korean",
+    "latin",
+    "spanish",
+    "swedish",
+    "portuguese",
+    "russian",
+    "norwegian"
+  ];
+
+  lans = [
+    "ar",
+    "bg",
+    "zh",
+    "cs",
+    "da",
+    "nl",
+    "en",
+    "fr",
+    "de",
+    "it",
+    "ja",
+    "ko",
+    "la",
+    "es",
+    "sw",
+    "pt",
+    "ru",
+    "no"
+  ];
+
+  button = createA("#", "click me");
+  button.style("padding", "12px");
+  button.style("border-style", "solid");
+  button.mousePressed(transAll);
+
+  content = select("#content");
+  container = select("#container");
+  arr = poem.split("*");
+  for (let i = 0; i < arr.length; i++) {
+    const line = createP(arr[i]);
+    line.parent(content);
+    lines.push(line);
+  }
+}
+
+function transAll() {
+  for (let i = 0; i < lines.length; i++) {
+    trans(arr[i], lines[i]);
+  }
+}
+
+function trans(line, element) {
+  fetch(
+    "https://translation.googleapis.com/language/translate/v2/?q=" +
+      line +
+      "&source=en&target=zh&key=" +
+      apiKey
+  )
+    .then(response => response.json())
+    .then(data => {
+      const middle = data.data.translations[0].translatedText;
+      fetch(
+        "https://translation.googleapis.com/language/translate/v2/?q=" +
+          middle +
+          "&source=zh&target=en&key=" +
+          apiKey
+      )
+        .then(response => response.json())
+        .then(data => {
+          element.elt.innerHTML = data.data.translations[0].translatedText;
+        })
+        .catch(error => console.error(error));
+    })
+    .catch(error => console.error(error));
+}
+
+function search(ele) {
+  if (event.key === "Enter") {
+    console.log("here");
+    const index = languages.indexOf(ele.value.toLowerCase());
+    if (index != -1) {
+      let newlan = createElement("div", languages[index].toUpperCase());
+      newlan.class("lan");
+      newlan.style("color", colors[int(random(0, 5))]);
+      newlan.parent(container);
+      tranlans.push(lans[index]);
+    } else {
+      ele.value = "";
+    }
+  }
+}
