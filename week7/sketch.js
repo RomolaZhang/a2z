6146ec607c83dfac6d9c07be73e0023342bacaf0
Chuyi Zhang <cz1302@nyu.edu>
2018-10-24 12:03:08 -0400

    Week7

---
+++ week7/sketch.js
@@ -0,0 +1,27 @@
+let lines;
+let markov
+
+function preload() {
+  lines = loadStrings('names.txt');
+}
+
+function setup() {
+  noCanvas();
+  let generateButton = select('#generate');
+  generateButton.mousePressed(generate);
+
+}
+
+
+function generate() {
+  let order = select('#order');
+
+  markov = new MarkovGeneratorWord(2, Math.floor(5+Math.random()*10));
+
+  for (let i = 0; i < lines.length; i++) {
+    markov.feed(lines[i]);
+  }
+  const results = select('#results');
+  results.value(markov.generate());
+  console.log(markov.generate());
+}
